f02fa53b2a183c044fbb74ad7f1185401e57e486
Lars Giere <larsgiere@gmail.com>
2014-09-07 08:36:48 +0200

    - Added: Batched file read in to cope with memory issues - Added: Adaptive learning rate decrease depending on number of rows already calculated - Added: Learning rate scaling by batch size (1/bs) - Changed: Standard values for learning rate & regularization parameter to improve chances for conversion for ad-challenge

--- ad_challenge_test.py
+++ ad_challenge_test.py
@@ -4,11 +4,17 @@ import scipy.stats as stats
 
 import algorithms # our own implementation of the algorithms
 
-path = './'
+trainpath = '/Volumes/INTENSO/'
+#trainpath = './'
+filename = trainpath+'train.csv'
 
-def convertfunc(value):
-    if value != np.nan:
-        return float(int(value,16))
+testpath = '/Volumes/INTENSO/'
+#testpath = './'
+testfilename = testpath+'test.csv'
+
+resultfilename = testpath+'result.csv'
+
+batch_length = 100000
 
 # fill 'nan': use the mean of the column
 def fillna_with_mean(mat):   
@@ -19,39 +25,106 @@ def fillna_with_mean(mat):
     #Place column means in the indices. Align the arrays using take
     mat[inds]=np.take(col_mean,inds[1])
 
-'''
-Read training data
-'''
+# determine the length of the file
+def file_len(fname):
+    with open(fname) as f:
+        for i, l in enumerate(f):
+            pass
+    return i + 1
+
+#filter to just use a portion of the file for calculations
+def filter_lines(f, start_val, end_val):
+    for i, line in enumerate(f):
+        if i >= start_val and i <= end_val:
+            yield line
+        elif i > end_val:
+            break
+
+# float conversion function to be used while reading in the file
+def convertfunc(value):
+    if value != np.nan:
+        return float(int(value,16))
+
+#columnd definition for conversion function usage
 conv = {col: convertfunc for col in range(1,41)}
-train_data = np.genfromtxt(path+'train_head.csv',delimiter=',',skip_header=1,dtype=float,converters=conv)
-# fill 'Nan' values with the mean of the corresponding column
-fillna_with_mean(train_data[:,2:])
-# define training labels and features
-train_labels   = train_data[:,1]
-train_features = train_data[:,2:]
 
-'''
-Read test data
-'''
-conv = {col: convertfunc for col in range(1,40)}
-test_data = np.genfromtxt(path+'test_head.csv',delimiter=',',skip_header=1,dtype=float,converters=conv)   
-# fill 'Nan' values with the mean of the corresponding column  
-fillna_with_mean(test_data[:,1:])
-# define test id and features
-test_id       = test_data[:,0]
-test_features = test_data[:,1:]
-
-
-'''
-Learn and test part
-'''
+#determine the training set file length
+file_length = file_len(filename)
 
 # initialize stochastic gradient descent class (home made)
-sgd = algorithms.StochasticGradientDescentMethod(train_labels,train_features)
-sgd.learn(10,True)
+sgd = algorithms.StochasticGradientDescentMethod()
 
-result = sgd.predict(test_id,test_features)
-print result[:20,:]
+'''
+Train with stochastic gradient descent
+'''
+start=1
+end=0
+
+while end < file_length:
+    end += batch_length
+    if start == 1:
+        initialize = True
+    else:
+        initialize = False
+        
+    with open(filename) as f:
+        train_data = np.genfromtxt(filter_lines(f, start, end),delimiter=',',skip_header=0,dtype=float,converters=conv)
+
+    #train_data = np.genfromtxt(path+'train_head.csv',delimiter=',',skip_header=1,dtype=float,converters=conv)
+    # fill 'Nan' values with the mean of the corresponding column
+    fillna_with_mean(train_data[:,2:])
+    # define training labels and features
+    train_labels   = train_data[:,1]
+    train_features = train_data[:,2:]
+
+    sgd.setTrainData(train_labels,train_features,initialize)
+    sgd.learn(1,True)
+    print ('Start: '+str(start)+'\nEnd: '+str(end))
+    start += batch_length
+
+
+'''
+Calculate Prediction with test Data
+'''
+
+#determine the training set file length
+test_file_length = file_len(testfilename)
+
+conv = {col: convertfunc for col in range(1,40)}
+
+start=1
+end=0
+
+with open(resultfilename, "w") as text_file:
+        text_file.write('Id,Predicted\n')
+
+while end < test_file_length:
+    end += batch_length
+    with open(testfilename) as f:
+        test_data = np.genfromtxt(filter_lines(f, start, end),delimiter=',',skip_header=0,dtype=float,converters=conv)   
+    
+    # fill 'Nan' values with the mean of the corresponding column  
+    fillna_with_mean(test_data[:,1:])
+    # define test id and features
+    test_id = test_data[:,0]
+    test_id = test_id.astype(int)
+    test_features = test_data[:,1:]
+
+    #Calculate the prediction    
+    result = sgd.predict(test_id,test_features)
+    res = np.rec.fromarrays((result[:,0].astype(int), result[:,1].astype(float)))
+    #result[:,1] = result[:,1].astype('float32')
+    with open(resultfilename, 'a') as f_handle:      
+        for item in res:
+            f_handle.write(str(item[0][0])+','+str(item[0][1])+'\n')  
+    
+    '''
+    #Write predictions to result file
+    with open(resultfilename,'a') as f_handle:
+        np.savetxt(f_handle, res, delimiter=",")
+    '''
+    print ('Start: '+str(start)+'\nEnd: '+str(end))
+    start += batch_length
 
 
 
--- algorithms.py
+++ algorithms.py
@@ -5,20 +5,38 @@ Created on Sun Aug 31 15:47:27 2014
 @author: Sweta
 """
 import numpy as np
+import scipy as sp
 
 class StochasticGradientDescentMethod():
     """ A class for the stochastic gradient descent method"""   
     
-    def __init__(self,train_labels,train_features):
+    def __init__(self,train_labels=np.zeros((1,1)),train_features=np.zeros((1,1))):
         self.label_vec = train_labels
         self.feature_mat = train_features
         self.weights_vec = np.zeros(self.feature_mat.shape[1])
         self.scaling_vec = np.linalg.norm(self.feature_mat,np.inf,axis=0)
         self.feature_mat = self.scale(self.feature_mat)
+        self.iter = 0
+        
+    def setTrainData(self,train_labels,train_features,init=False):
+        self.label_vec = train_labels
+        self.feature_mat = train_features
+        if init:
+            self.weights_vec = np.zeros(self.feature_mat.shape[1])
+            self.scaling_vec = np.linalg.norm(self.feature_mat,np.inf,axis=0)
+        self.feature_mat = self.scale(self.feature_mat)
         
     def scale(self,mat):         
          scaled_mat = (mat.T/self.scaling_vec[:,None]).T
          return scaled_mat
+         
+    def logLossError(self, act, pred):
+        epsilon = 1e-15
+        pred = sp.maximum(epsilon, pred)
+        pred = sp.minimum(1-epsilon, pred)
+        ll = sum(act*sp.log(pred) + sp.subtract(1,act)*sp.log(sp.subtract(1,pred)))
+        ll = ll * -1.0/len(act)
+        return ll
         
     def logistic_loss_function_gradient(self,x,y,p):
         '''
@@ -31,17 +49,19 @@ class StochasticGradientDescentMethod():
         l1 = 1 + l2 # outer derivative of log
         return ( (-1.0) * l2 * y * x) / l1
         
-    def learn(self,no_iter,shuffle):
+    def learn(self,no_iter,shuffle, eta=0.001, theta=0.000001):
         '''
         note: logistic loss function is used by default
         no_iter : how many times the algorithm is applied to the training set
         shuffle : boolean indicating if training shall be shuffled before each iteration
         '''            
-        eta   = 0.001  # learning rate
-        theta = 0.7    # regularization parameter
+        #eta   = 0.001  # learning rate
+        #theta = 0.7    # regularization parameter
 
         for it in range(0,no_iter):
+            res=self.feature_mat.dot(self.weights_vec)
             print('iteration : '+str(it))
+            print('Error with latest weights:'+str(self.logLossError(self.label_vec,res)))
             if shuffle == True:
                 combi_mat =  np.append(self.label_vec[:,None],self.feature_mat,1)
                 np.random.shuffle( combi_mat )
@@ -49,9 +69,11 @@ class StochasticGradientDescentMethod():
                 self.label_vec   = combi_mat[:,0]
             # run SGD for every sample
             for ind, row in enumerate(self.feature_mat):
+                self.iter += 1
                 p = np.inner(self.weights_vec,row) # compute probability
-                self.weights_vec -= eta * theta*self.weights_vec #l2 Regularization
-                self.weights_vec -= eta * self.logistic_loss_function_gradient(row,self.label_vec[ind],p)
+                alpha = (1/(float(self.label_vec.shape[0]*(1+eta*theta*self.iter))))*eta
+                self.weights_vec -= alpha * theta*self.weights_vec #l2 Regularization
+                self.weights_vec -= alpha * self.logistic_loss_function_gradient(row,self.label_vec[ind],p)
                 
         return self.weights_vec
         
@@ -61,8 +83,9 @@ class StochasticGradientDescentMethod():
         if np.linalg.norm(self.weights_vec) == 0:
             raise ValueError('All weights are zero! (Possible reason: learn() was not called.)')
         # scale feature mat by scaling_vec (the same as used for the training set)
-        test_features = self.scale(test_features)   
-        return ( np.matrix([ test_id, np.dot(test_features,self.weights_vec) ]) ).T
+        test_features = self.scale(test_features)
+        return ( np.matrix([ test_id.astype(int), np.dot(test_features,self.weights_vec) ])).T
+        
         
      
         
